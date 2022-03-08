e0974f9b4d6c427080f82c1c5da4aa96be5e99ab
progaurab <progaurab@gmail.com>
2022-03-08 10:49:44 +0530

    updated ex5

--- ex5/www/js/index.js
+++ ex5/www/js/index.js
@@ -1,29 +1,24 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
-
+document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+    function checkConnection() {
+        var networkState = navigator.connection.type;
+        if (networkState !== Connection.NONE) {
+            alert("You are connected to Internet");
+        } else {
+            alert("You are Offline");
+        }
+        
+        document.getElementById("ConnectionType").innerHTML = networkState
+        document.getElementById("UNKNOWN").innerHTML = Connection.UNKNOWN
+        document.getElementById("ETHERNET").innerHTML = Connection.ETHERNET
+        document.getElementById("WIFI").innerHTML = Connection.WIFI
+        document.getElementById("CELL_2G").innerHTML = Connection.CELL_2G
+        document.getElementById("CELL_3G").innerHTML = Connection.CELL_3G
+        document.getElementById("CELL_4G").innerHTML = Connection.CELL_4G
+        document.getElementById("CELL").innerHTML = Connection.CELL
+        document.getElementById("aConnection").innerHTML = Connection.NONE
+    }
+
+    checkConnection();
 }
